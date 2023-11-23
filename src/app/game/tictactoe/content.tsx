bd8598c9b8029286969b766ea758149d9997bf53
Janthanat Thanintantrakun <73385307+juntthanat@users.noreply.github.com>
2023-11-23 19:57:39 +0700

    update: reset button ui

--- src/app/game/tictactoe/content.tsx
+++ src/app/game/tictactoe/content.tsx
@@ -1,5 +1,5 @@
-'use client'
-import {useState, useEffect} from "react";
+"use client";
+import { useState, useEffect } from "react";
 
 export default function TictactoeContent() {
   const [numberOfColumn, setNumberOfColumn] = useState<number>();
@@ -8,25 +8,44 @@ export default function TictactoeContent() {
 
   const columnChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     setNumberOfColumn(Number(e.target.value));
-  }
-  const rowChangeHandler = (event:number) => {
-    setNumberOfRow(event);
-  }
-  const winConditionChangeHandler = (event:number) => {
-    setNumberOfWinCondition(event);
+  };
+  const rowChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNumberOfRow(Number(e.target.value));
+  };
+  const winConditionChangeHandler = (
+    e: React.ChangeEvent<HTMLInputElement>
+    ) => {
+    setNumberOfWinCondition(Number(e.target.value));
+  };
+  const resetButtonHandler = () => {
+    console.log("Reset Button Pressed")
   }
 
-
-  function createTicTacToeTable(){
+  function createTicTacToeTable() {
 
   }
 
   return (
     <div className="h-full w-full">
       <div className="h-1/5 w-full bg-red-100 flex justify-evenly items-center">
-        <input type="number" placeholder="Column" onChange={columnChangeHandler}></input>
-        <input type="number" placeholder="Row"></input>
-        <input type="number" placeholder="Win Condition"></input>
+        <input
+          type="number"
+          placeholder="Column"
+          onChange={columnChangeHandler}
+        ></input>
+        <input
+          type="number"
+          placeholder="Row"
+          onChange={rowChangeHandler}
+        ></input>
+        <input
+          type="number"
+          placeholder="Win Condition"
+          onChange={winConditionChangeHandler}
+        ></input>
+        <div className="h-2/5 w-1/6 bg-gray-300 hover:bg-gray-400 active:bg-gray-500 rounded border border-black flex justify-center items-center select-none" onClick={resetButtonHandler}>
+          Reset
+        </div>
       </div>
       <div className="h-4/5 w-full bg-blue-100"></div>
     </div>
