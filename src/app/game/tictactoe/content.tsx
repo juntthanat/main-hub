37fd7c6a5b4a426d830478a0e8e27ad5c0484e9c
Janthanat Thanintantrakun <73385307+juntthanat@users.noreply.github.com>
2023-11-24 14:39:39 +0700

    feaet: tictactoe column

--- src/app/game/tictactoe/content.tsx
+++ src/app/game/tictactoe/content.tsx
@@ -2,28 +2,42 @@
 import { useState, useEffect } from "react";
 
 export default function TictactoeContent() {
-  const [numberOfColumn, setNumberOfColumn] = useState<number>();
-  const [numberOfRow, setNumberOfRow] = useState<number>();
-  const [numberOfWinCondition, setNumberOfWinCondition] = useState<number>();
+  const [numberOfColumn, setNumberOfColumn] = useState<JSX.Element[]>();
+  const [numberOfRow, setNumberOfRow] = useState<number>(3);
+  const [numberOfWinCondition, setNumberOfWinCondition] = useState<number>(3);
+  const [ticTacToeTable, setTicTacToeTable] = useState<JSX.Element[]>();
 
   const columnChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setNumberOfColumn(Number(e.target.value));
+    let col = [];
+    for (
+      let columnIndex = 0;
+      columnIndex < Number(e.target.value);
+      columnIndex++
+    ) {
+      col.push(<td>Button</td>);
+    }
+    setNumberOfColumn(col);
   };
   const rowChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     setNumberOfRow(Number(e.target.value));
   };
   const winConditionChangeHandler = (
     e: React.ChangeEvent<HTMLInputElement>
-    ) => {
+  ) => {
     setNumberOfWinCondition(Number(e.target.value));
   };
   const resetButtonHandler = () => {
-    console.log("Reset Button Pressed")
-  }
+    console.log("Reset Button Pressed");
+  };
 
-  function createTicTacToeTable() {
-
-  }
+  const createTicTacToeTable = () => {
+    let initialTable = [];
+    for (let rowIndex = 0; rowIndex < numberOfRow; rowIndex++) {
+      <tr></tr>;
+    }
+    initialTable.push(<div>hello world</div>);
+    setTicTacToeTable(initialTable);
+  };
 
   return (
     <div className="h-full w-full">
@@ -43,11 +57,24 @@ export default function TictactoeContent() {
           placeholder="Win Condition"
           onChange={winConditionChangeHandler}
         ></input>
-        <div className="h-2/5 w-1/6 bg-gray-300 hover:bg-gray-400 active:bg-gray-500 rounded border border-black flex justify-center items-center select-none" onClick={resetButtonHandler}>
+        <div
+          className="h-2/5 w-1/6 bg-gray-300 hover:bg-gray-400 active:bg-gray-500 rounded border border-black flex justify-center items-center select-none"
+          onClick={resetButtonHandler}
+        >
           Reset
         </div>
       </div>
-      <div className="h-4/5 w-full bg-blue-100"></div>
+      <div className="h-4/5 w-full bg-blue-100">
+        {ticTacToeTable}
+        <table>
+          <tr>
+            {numberOfColumn}
+          </tr>
+          <tr>
+            {numberOfColumn}
+          </tr>
+        </table>
+      </div>
     </div>
   );
 }
