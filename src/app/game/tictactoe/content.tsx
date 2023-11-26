4044f9d5e19d8c8073955c648ab5a61d98c290f2
Janthanat Thanintantrakun <73385307+juntthanat@users.noreply.github.com>
2023-11-26 22:06:32 +0700

    update: change row and col to matrix

--- src/app/game/tictactoe/content.tsx
+++ src/app/game/tictactoe/content.tsx
@@ -2,32 +2,29 @@
 import { useState, useEffect } from "react";
 
 export default function TictactoeContent() {
-  const [numberOfColumn, setNumberOfColumn] = useState<JSX.Element[]>();
-  const [numberOfRow, setNumberOfRow] = useState<JSX.Element[]>();
   const [numberOfWinCondition, setNumberOfWinCondition] = useState<number>(3);
   const [ticTacToeTable, setTicTacToeTable] = useState<JSX.Element[]>();
 
-  const columnChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    let col = [];
-    for (
-      let columnIndex = 0;
-      columnIndex < Number(e.target.value);
-      columnIndex++
-    ) {
-      col.push(<td>Button</td>);
-    }
-    setNumberOfColumn(col);
-  };
-  const rowChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    let row = [];
-    for (
+  const matrixChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let matrixTable = [];
+    let rowNum = []
+    for(
       let rowIndex = 0;
       rowIndex < Number(e.target.value);
       rowIndex++
     ) {
-      row.push(<tr>{numberOfColumn}</tr>)
+      // console.log("row index "+rowIndex)
+      rowNum.push(<div key={rowIndex}>{rowIndex}</div>)
     }
-    setNumberOfRow(row);
+    for (
+      let matrixIndex = 0;
+      matrixIndex < Number(e.target.value);
+      matrixIndex++
+    ) {
+      // console.log("matrix index "+matrixIndex)
+      matrixTable.push(<div className="flex" key={matrixIndex}>{rowNum}</div>);
+    }
+    setTicTacToeTable(matrixTable);
   };
   const winConditionChangeHandler = (
     e: React.ChangeEvent<HTMLInputElement>
@@ -38,31 +35,15 @@ export default function TictactoeContent() {
     console.log("Reset Button Pressed");
   };
 
-  // const createTicTacToeTable = () => {
-  //   let initialTable = [];
-  //   for (let rowIndex = 0; rowIndex < numberOfRow; rowIndex++) {
-  //     <tr></tr>;
-  //   }
-  //   initialTable.push(<div>hello world</div>);
-  //   setTicTacToeTable(initialTable);
-  // };
-
   return (
     <div className="h-full w-full">
       <div className="h-1/5 w-full bg-red-100 flex justify-evenly items-center">
         <input
           type="number"
-          placeholder="Column"
+          placeholder="Matrix"
           defaultValue={0}
           min={0}
-          onChange={columnChangeHandler}
-        ></input>
-        <input
-          type="number"
-          placeholder="Row"
-          defaultValue={0}
-          min={0}
-          onChange={rowChangeHandler}
+          onChange={matrixChangeHandler}
         ></input>
         <input
           type="number"
@@ -75,24 +56,13 @@ export default function TictactoeContent() {
           className="h-2/5 w-1/6 bg-gray-300 hover:bg-gray-400 active:bg-gray-500 rounded border border-black flex justify-center items-center select-none"
           onClick={resetButtonHandler}
         >
-          Create
-        </div>
-        <div
-          className="h-2/5 w-1/6 bg-gray-300 hover:bg-gray-400 active:bg-gray-500 rounded border border-black flex justify-center items-center select-none"
-          onClick={resetButtonHandler}
-        >
           Reset
         </div>
       </div>
       <div className="h-4/5 w-full bg-blue-100">
-        {ticTacToeTable}
-        <table>
-          <tbody>
-            {numberOfRow}
-            {/* <tr>{numberOfColumn}</tr>
-            <tr>{numberOfColumn}</tr> */}
-          </tbody>
-        </table>
+        <div>
+            {ticTacToeTable}
+        </div>
       </div>
     </div>
   );
